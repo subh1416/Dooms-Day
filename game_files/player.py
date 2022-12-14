32f258bdbcda4c54fb593b6a3b042f851175e1ab
subh1416 <subhadipsamanta1416@gmail.com>
2022-12-14 21:48:37 +0530

    initial done

--- game_files/player.py
+++ game_files/player.py
@@ -19,11 +19,11 @@ class Player:
 
         keys = pg.key.get_pressed()
         if keys[pg.K_w]:
-            dx += speed_sin
-            dy += speed_cos
+            dx += speed_cos
+            dy += speed_sin
         if keys[pg.K_s]:
-            dx += -speed_sin
-            dy += -speed_cos
+            dx += -speed_cos
+            dy += -speed_sin
         if keys[pg.K_a]:
             dx += speed_sin
             dy += -speed_cos
@@ -33,11 +33,11 @@ class Player:
 
         self.check_wall_collision(dx,dy)
 
-        if keys[pg.K_LEFT]:
-            self.angle += PLAYER_ROT_SPEED * self.game.delta_time
+        # if keys[pg.K_LEFT]:
+        #     self.angle -= PLAYER_ROT_SPEED * self.game.delta_time
 
-        if keys[pg.K_RIGHT]:
-            self.angle += PLAYER_ROT_SPEED * self.game.delta_time
+        # if keys[pg.K_RIGHT]:
+        #     self.angle += PLAYER_ROT_SPEED * self.game.delta_time
         self.angle %= math.tau
         
         
@@ -45,20 +45,31 @@ class Player:
         return (x,y) not in self.game.map.world_map
     
     def check_wall_collision(self,dx,dy):
-        if self.check_wall(int(self.x + dx),int(self.y )):
+        scale = PLAYER_SIZE_SCALE / self.game.delta_time
+        if self.check_wall(int(self.x + dx*scale),int(self.y )):
             self.x += dx
-        if self.check_wall(int(self.x ),int(self.y + dy)):
+        if self.check_wall(int(self.x ),int(self.y + dy*scale)):
             self.y += dy
 
 
     def draw(self):
-        #pg.draw.line(self.game.screen, 'yellow', (self.x*100, self.y*100),
-        #           (self.x*100 + WIDTH*math.cos(self.angle),
-        #             self.x*100 + WIDTH*math.sin(self.angle)), 2)
+        pg.draw.line(self.game.screen, 'yellow', (self.x*100, self.y*100),
+                  (self.x*100 + WIDTH*math.cos(self.angle),
+                    self.x*100 + WIDTH*math.sin(self.angle)), 2)
         pg.draw.circle(self.game.screen,'green',(self.x * 100,self.y * 100),15)
+        
+    def mouse_control(self):
+        mx, my = pg.mouse.get_pos()
+        if mx < MOUSE_BORDER_LEFT or mx > MOUSE_BORDER_RIGHT:
+            pg.mouse.set_pos([HALF_WIDTH, HALF_HEIGHT])
+        self.rel = pg.mouse.get_rel()[0]
+        self.rel = max(-MOUSE_MAX_REL, min(MOUSE_MAX_REL, self.rel))
+        self.angle += self.rel * MOUSE_SENSITIVITY * self.game.delta_time
+        
 
     def update(self):
         self.movement()
+        self.mouse_control()
 
     @property
     def pos(self):
